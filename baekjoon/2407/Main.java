2aa0b8383ddb388cc2e82030aaa2bc1aeff5c3cb
BEMELON <hgdkim2@naver.com>
2023-01-18 10:16:19 +0900

    :tada: Solve : BJ 2407

---
+++ baekjoon/2407/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine()); 
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        BigInteger n1 = BigInteger.ONE;
+        BigInteger n2 = BigInteger.ONE;
+
+        for (int i = 0; i < m; i++) {
+            n1 = n1.multiply(new BigInteger(String.valueOf(n - i)));
+            n2 = n2.multiply(new BigInteger(String.valueOf(i + 1)));
+        }
+
+        BigInteger answer = n1.divide(n2);
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+}
