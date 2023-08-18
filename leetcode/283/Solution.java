45f8dbe38590286b1bd1e3f31dddc7fbaae55303
황규도 <hgdkim2@naver.com>
2023-08-18 21:58:29 +0900

    :recycle: Refactoring: fill zeros after non-zeros

--- leetcode/283/Solution.java
+++ leetcode/283/Solution.java
@@ -1,20 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p1 = 0;
-        int p2 = p1 + 1;
-
-        while (p2 < nums.length) {
-            if (nums[p1] != 0) {
-                p1++;
-            } else if (nums[p1] == 0 && nums[p2] != 0) {
-                int temp = nums[p1];
-                nums[p1] = nums[p2];
-                nums[p2] = temp;
-
-                p1++;
+        int p = 0;
+        for(int num: nums) {
+            if (num != 0) {
+                nums[p++] = num;
             }
-
-            p2++;
         }
+
+        while (p < nums.length)
+            nums[p++] = 0;
     }
 }
