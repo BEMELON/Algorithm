8074293a11ea209f09c61919ae4459520f90f7b9
황규도 <hgdkim2@naver.com>
2023-08-20 00:06:01 +0900

    :recycle: refactoring: Add Union-By-Rank optimization

--- theory/DSU/Graph.java
+++ theory/DSU/Graph.java
@@ -3,13 +3,16 @@ import java.util.List;
 
 public class Graph {
     public final List<Integer> parents;
+    public final List<Integer> rank;
 
     public Graph(int n) {
         this.parents = new ArrayList<>();
+        this.rank = new ArrayList<>();
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++) {
             this.parents.add(-1);
-
+            this.rank.add(1);
+        }
     }
 
     public int find(int v) {
@@ -30,8 +33,17 @@ public class Graph {
             return ;
         }
 
-        // P2의 최상위 부모를 P1으로 설정
-        this.parents.set(p2, p1);
+        int r1 = this.rank.get(p1);
+        int r2 = this.rank.get(p2);
+
+        // P2가 더 큰 그룹인 경우
+        if (r1 < r2) {
+            this.parents.set(p1, p2); // P1의 부모를 P2로
+            this.rank.set(p2, r1 + r2);
+        } else {
+            this.parents.set(p2, p1);
+            this.rank.set(p1, r1 + r2);
+        }
     }
 
     public int getParent(int v) {
@@ -59,6 +71,16 @@ public class Graph {
 
         System.out.println("==> Union(3, 4)");
         g.union(3, 4);
+
+        System.out.println("==> 노드의 그룹");
+        for(int i = 0; i < 5; i++) {
+            System.out.print(g.getParent(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println("==> Union(2, 3)");
+        g.union(2, 3);
+
         System.out.println("==> 노드의 그룹");
         for(int i = 0; i < 5; i++) {
             System.out.print(g.getParent(i) + " ");
