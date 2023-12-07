963c095748fec43cbfb54a46969c24ff09f5cf00
황규도 <hgdkim2@naver.com>
2023-12-07 16:16:24 +0900

    :tada: Solve BJ 10830

---
+++ baekjoon/10830/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer stk;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
