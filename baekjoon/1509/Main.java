104973f5f858907285d51208a680fadee0633725
황규도 <hgdkim2@naver.com>
2023-12-27 12:25:55 +0900

    :tada: Solve BJ 1509

--- baekjoon/1509/Main.java
+++ baekjoon/1509/Main.java
@@ -1,56 +1,56 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     static StringTokenizer stk;
-    static int result;
-    static String word;
-    static boolean[][] palindrome;
-    static int[] dp;
+    static long MOD = 1_000_000_000L;
+
+    static long[][][] dp;
+
+    static int n;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        word = br.readLine();
-        result = word.length();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // dp[i][j] = i ~ j 번째의 문자열이 Palindrome 한지
-        palindrome = new boolean[word.length()][word.length()];
+        n = Integer.parseInt(br.readLine());
 
-        // CASE 1
-        for(int i = 0; i < word.length(); i++) {
-            palindrome[i][i] =  true;
+        // dp[i][j][k] = k의 자리를 방문했으면서 i번째 자리에 j가 올 수 있는 경우의 수
+        dp = new long[n][10][1 << 10];
+
+        // 1자리
+        for(int i = 1; i < 10; i++) {
+            dp[0][i][1 << i] = 1;
         }
 
-        // CASE 2
-        for(int i = 0; i < word.length() - 1; i++) {
-            if (word.charAt(i) == word.charAt(i + 1)) {
-                palindrome[i][i + 1] = true;
-            }
-        }
-
-        // CASE 3
-        for(int len = 3; len <= word.length(); len++) {
-            for(int start = 0; start + len - 1  < word.length(); start++) {
-                int end = start + len - 1;
-
-                if (word.charAt(start) == word.charAt(end) && palindrome[start + 1][end - 1]) palindrome[start][end] = true;
-            }
-        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < 10; j++) {
+                for(int k = 0; k < 1024; k++) {
 
 
-        dp = new int[word.length() + 1];
+                    if (j - 1 >= 0)
+                        dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
 
-        for(int end = 1; end <= word.length(); end++) {
-            dp[end] = Integer.MAX_VALUE;
-            for(int start = 1; start <= end; start++) {
-                if (palindrome[start - 1][end - 1]) {
-                    dp[end] = Math.min(dp[end], dp[start - 1] + 1);
+
+                    if (j + 1 <= 9)
+                        dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
+
+
+                    dp[i][j][k | (1 << j)] %= MOD;
                 }
             }
         }
-        
-        System.out.println(dp[word.length()]);
-    }
+        long result = 0;
+        for(int i = 0; i < 10; i++) {
+            result += dp[n - 1][i][1023];
+            result %= MOD;
+        }
 
+        bw.write(result + "\n");
+
+        br.close();
+        bw.close();
+    }
 }
